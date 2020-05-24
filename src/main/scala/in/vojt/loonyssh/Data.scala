6f4273c34096d67e4377374a79cb00b196fc04d6
Vojtech Letal <vojtech.letal@blindspot.ai>
2020-05-24 11:09:14 +0200

    define error

--- src/main/scala/in/vojt/loonyssh/Data.scala
+++ src/main/scala/in/vojt/loonyssh/Data.scala
@@ -1,5 +1,9 @@
 package in.vojt.loonyshh
 
+import scala.compiletime._
+import java.io._
+import scala.reflect.ClassTag
+
 /**
     A string containing a comma-separated list of names.  A name-list
     is represented as a uint32 containing its length (number of bytes
@@ -12,3 +16,8 @@ case class NameList(names:List[String])
 
 object NameList:
     given reader as SSHReader[NameList] = SSHReader[String].map(s => NameList(s.split(",").toList))
+
+
+opaque type LSeq[L<:Int, V] = Seq[V]
+object LSeq:
+    def apply[L<:Int, V](seq:Seq[V]):LSeq[L,V] = seq
--- src/main/scala/in/vojt/loonyssh/Reader.scala
+++ src/main/scala/in/vojt/loonyssh/Reader.scala
@@ -7,8 +7,13 @@ import scala.reflect.ClassTag
 import scala.deriving._
 import scala.compiletime._
 
-trait SSHReader[V]: // reader monad?
-    def read(is: BufferedInputStream): V // should be Either
+enum Error:
+    case Unexpected(e:Exception)
+    
+type ErrOr[V] = Either[Error, V]
+
+trait SSHReader[V]: // IO / Reader monad?
+    def read(is: BufferedInputStream): V // should return ErrOr[V]
 
     def map[W](f:V=>W):SSHReader[W] = new SSHReader[W]:
         def read(is: BufferedInputStream): W = f(SSHReader.this.read(is))
@@ -16,17 +21,8 @@ trait SSHReader[V]: // reader monad?
     def flatMap[W](f:V=>SSHReader[W]):SSHReader[W] = new SSHReader[W]:
         def read(is: BufferedInputStream): W = f(SSHReader.this.read(is)).read(is)
 
-
-opaque type LSeq[L<:Int, V] = Seq[V]
-object LSeq:
-    def apply[L<:Int, V](seq:Seq[V]):LSeq[L,V] = seq
-
 object SSHReader:
     inline def apply[V](using impl: SSHReader[V]):SSHReader[V] = impl
-
-    inline given lSeq[L<:Int, T:ClassTag] as SSHReader[LSeq[L,T]] = new SSHReader:
-        def read(is: BufferedInputStream): LSeq[L, T] = 
-            LSeq[L,T](Array.fill(constValue[L])(summonInline[SSHReader[T]].read(is)).toSeq)
 
     given intReader as SSHReader[Int] = is => ByteBuffer.wrap(is.readNBytes(4)).getInt
     given byteReader as SSHReader[Byte] = is => is.read().toByte
@@ -39,10 +35,15 @@ object SSHReader:
         a <- arrayReader(n)
     } yield new String(a)
 
+    // should but can not be in Data, because of the inline restriction
+    inline given lSeq[L<:Int, T:ClassTag] as SSHReader[LSeq[L,T]] = new SSHReader:
+        def read(is: BufferedInputStream): LSeq[L, T] = 
+            LSeq[L,T](Array.fill(constValue[L])(summonInline[SSHReader[T]].read(is)).toSeq)
+
     inline given derived[V](using m: Mirror.ProductOf[V]) as SSHReader[V] = new SSHReader:
-        def read(is: BufferedInputStream): V = 
-            val parsed = readProduct[m.MirroredElemTypes](is)(0)
-            m.fromProduct(parsed.asInstanceOf[Product]).asInstanceOf[V]
+        def read(is: BufferedInputStream): V = m.fromProduct(
+            readProduct[m.MirroredElemTypes](is)(0).asInstanceOf
+        ).asInstanceOf[V]
         
     inline private def readProduct[T](is: BufferedInputStream)(i:Int):Tuple = inline erasedValue[T] match
         case _: (t *: ts) => summonInline[SSHReader[t]].read(is) *: readProduct[ts](is)(i+1)
